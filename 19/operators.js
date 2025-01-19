f208da0e1cd418e158de284fabc719209afc7ff5
William Chong <heathchong.gmail.com>
2025-01-19 15:47:26 +0800

    19/1

---
+++ 19/operators.js
@@ -0,0 +1,6 @@
+let hasCheesePromotion = true;
+let buyMilk = true; // 無論是否有促銷，都會買牛奶
+let buyCheese = hasCheesePromotion ? true : false; // 如果有促銷，買芝士
+
+console.log('買牛奶:', buyMilk);
+console.log('買芝士:', buyCheese);
